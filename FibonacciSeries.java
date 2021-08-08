ea032586462bf102c27588bc8cc728905b856504
Puhzhenthi <88572023+Puhzhenthi@users.noreply.github.com>
2021-08-08 12:19:02 +0530

    Add files via upload

---
+++ FibonacciSeries.java
@@ -0,0 +1,17 @@
+package week1.day1;
+
+public class FibonacciSeries {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int firstNumber=0,secondNumber=1,range=8;
+System.out.println(firstNumber);
+for (int i = 1; i <= range; i++) {
+	int sum=firstNumber+secondNumber;
+	System.out.println(sum);
+	firstNumber=secondNumber;
+	secondNumber=sum;
+}
+	}
+
+}
---
+++ PrimeNumber.java
@@ -0,0 +1,33 @@
+package week1.day1;
+
+public class PrimeNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int num=8;
+boolean flag= false;
+for (int i = 2; i <=num /2; i++) {
+	if (num%i==0) {
+		flag=true;
+	if (!flag) {
+		System.out.println(num + "is a prime number.");
+	} else {
+System.out.println(num +" is not a prime num.");
+	}				
+				
+			}
+		}
+		
+	} 
+		
+	
+		
+	
+		
+}
+	
+
+
+	
+
+
